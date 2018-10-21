7be0baedd35edfd857f6d1e9702ac0bc727958a8
zs-home <jisung87kr@gamil.com>
2018-10-22 01:19:39 +0900

    added to ajax function

---
+++ calendar.php
@@ -0,0 +1,41 @@
+<?php
+    $input = $_POST['date']; // 구하고자하는 날짜
+    $totalDay = date('t', $input); //이달의 일수
+    $firstDay = date('w', strtotime(date("Ym", $input)."01")); //1일의 요일
+    $lastDay = date('w', strtotime(date("Ym", $input).$totalDay)); //마지막 요일
+    $totalWeek = ceil(($firstDay+$totalDay)/7); //이달의 주수
+
+    $prev = date("Ymd", strtotime("-1 month", $input)); //이전달
+    $next = date("Ymd", strtotime("+1 month", $input)); //다음달
+ ?>
+ <h1><?php echo date('Ymd', $input); ?></h1>
+<table>
+    <thead>
+        <tr>
+            <th>일</th>
+            <th>월</th>
+            <th>화</th>
+            <th>수</th>
+            <th>목</th>
+            <th>금</th>
+            <th>토</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $day = 1; //일수 출력을 위한 변수 ?>
+        <?php for ($row = 1; $row <= $totalWeek; $row++) { //총 주수 만큼 행 생성 ?>
+            <tr>
+                <?php
+                for ($col = 0; $col < 7; $col++) { // 열생성
+                    echo "<td>";
+                    if(! ( ($row == 1 && $col < $firstDay) || ($row == $totalWeek && $col > $lastDay) ) ){ // 행이 1이고 열이 첫날보다 작거나 행이 총주수와 같고 열이 마지막날보다 큰경우가 아닐때만 요일생성
+                        echo $day;
+                        $day++;
+                    }
+                    echo "</td>";
+                }
+                 ?>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
--- index.php
+++ index.php
@@ -3,44 +3,39 @@
     <head>
         <meta charset="utf-8">
         <title>달력</title>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     </head>
     <body>
-<?php
-    $input = strtotime('2018-11-21'); // 구하고자하는 날짜
-    $totalDay = date('t', $input); //이달의 일수
-    $firstDay = date('w', strtotime(date("Ym", $input)."01")); //1일의 요일
-    $lastDay = date('w', strtotime(date("Ym", $input).$totalDay)); //마지막 요일
-    $totalWeek = ceil(($firstDay+$totalDay)/7); //이달의 주수
- ?>
-    <table>
-        <thead>
-            <tr>
-                <th>일</th>
-                <th>월</th>
-                <th>화</th>
-                <th>수</th>
-                <th>목</th>
-                <th>금</th>
-                <th>토</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php $day = 1; //일수 출력을 위한 변수 ?>
-            <?php for ($row = 1; $row <= $totalWeek; $row++) { //총 주수 만큼 행 생성 ?>
-                <tr>
-                    <?php
-                    for ($col = 0; $col < 7; $col++) { // 열생성
-                        echo "<td>";
-                        if(! ( ($row == 1 && $col < $firstDay) || ($row == $totalWeek && $col > $lastDay) ) ){ // 행이 1이고 열이 첫날보다 작거나 행이 총주수와 같고 열이 마지막날보다 큰경우가 아닐때만 요일생성
-                            echo $day;
-                            $day++;
+        <div class="box">
+            <input type="button" name="prev" value="prev" class="prev">
+            <h1></h1>
+            <input type="button" name="next" value="next" class="next">
+        </div>
+        <div id="calendar"></div>
+        <script type="text/javascript">
+            $(document).ready(function(){
+
+                function getCal(_date){
+                    var _date = _date ? _date : new Date().getTime()/1000;
+                    $.ajax({
+                        url: "./calendar.php",
+                        type: "POST",
+                        cache: false,
+                        async: false,
+                        data: {
+                            date : _date
+                        },
+                        success: function(e){
+                            $("#calendar").append(e);
                         }
-                        echo "</td>";
-                    }
-                     ?>
-                </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+                    });
+                }
+                getCal();
+
+                $(".box input").click(function(){
+
+                });
+            });
+        </script>
     </body>
 </html>
